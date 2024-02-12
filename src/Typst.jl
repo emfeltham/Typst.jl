d62a1bd92564e085f1d1aa2866c0436162df05f8
emfeltham <eric.feltham@yale.edu>
2024-02-12 12:37:42 -0500

    remove Makie

--- src/Typst.jl
+++ src/Typst.jl
@@ -4,8 +4,9 @@ module Typst
 Maybe, it would be better to save figures outside and separately call here
 to generate the typst content. that way, it would be agnostic, and we wouldn't
 need a huge dependency.
+-> done
 =#
-import CairoMakie: Figure, save
+# import CairoMakie: Figure, save
 
 using DataFrames
 
