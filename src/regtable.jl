f84b0009708a58524e67dfa7e661111c6e0abf4f
emfeltham <eric.feltham@yale.edu>
2024-02-14 09:16:10 -0500

    update

--- src/regtable.jl
+++ src/regtable.jl
@@ -8,15 +8,39 @@
 Cell-based regression table for typst. Basic units are `TableX` abstract types,
 which are printed and combined with header information and a caption to form a
 formatted ".typ" file.
+
+P-value key symbols must include escape characters as appropriate for printing
+strings in Julia.
 """
 function regtable_typ(
     ms, filename;
     modeltitles = nothing, caption = nothing, roundvals = 3,
-    understat = "se"
+    understat = "se", col1width = 10,
+    pvalkey = (
+        values = [0.1, 0.05, 0.005, 0.001],
+        symbols = [
+            "super[\$+\$]", "super[\$star.op\$]",
+            "super[\$star.op star.op\$]", "super[\$star.op star.op star.op\$]"
+        ]
+    )
 )
 
     # indentation for print formatting
     tb = "    ";
+
+    # p-stars and key
+    if !all(diff(pvalkey.values) .< 0)
+        error("p-value key is wrong")
+    end
+
+    pvks = pvalkey.symbols .* ", "
+    pstatement = "// pkey \n" * "#let ps = " * "(" * reduce(*, pvks) * ")" * "\n \n"
+    
+    
+    nts = ["ps.at(" * string(i-1) * ")" * "\$p<" * string(v) * "\$" for (i, v) in enumerate(pvalkey.values)];
+    nts = "#" .* nts;
+    nts[1:(end-1)] = nts[1:(end-1)] .* "; ";
+    pnote = "Note: " * reduce(*, nts); # bottom of table
     
     cnames = (unique∘reduce)(vcat, [coefnames(m) for m in ms]);
     ms_l = length(ms);
@@ -30,10 +54,30 @@ function regtable_typ(
 
     # extract model information
     stats = [nobs, r2, adjr2, aic, bic];
+    statnames = Dict( # nice names for common statistics
+        "nobs" => "N",
+        "r2" => "\$R^2\$",
+        "adjr2" => "Adjusted \$R^2\$",
+        "aic" => "AIC",
+        "bic" => "BIC"
+    );
+
     mfos = ModelInfo[];
     _modelinfos!(mfos, ms, stats, roundvals, rndp)
     
     colwidths = reduce(*, ["coliwidth, " for _ in 1:ms_l]);
+
+    mxname = 0
+    for mfo in mfos
+        for k in keys(mfo.coef)
+            mxname = max(mxname, length(k)) 
+        end
+        for k in keys(mfo.stats)
+            k_ = get(statnames, k, k)
+            mxname = max(mxname, length(k_)) 
+        end
+    end
+
     cols = "columns: " * "(col1width, " * colwidths * ")";
 
     # options arguments to gridx
@@ -45,7 +89,10 @@ function regtable_typ(
 
     # generate the body of the table (coefficients, statistics)
     cells = TableX[];
-    regtable_content!(cells, mfos, cnames, modeltitles, stats, understat);
+    regtable_content!(
+        cells, mfos, cnames, modeltitles,
+        stats, understat, pvalkey, pnote, statnames
+    );
 
     cells_p = [print(cell) for cell in cells];
 
@@ -65,16 +112,29 @@ function regtable_typ(
 
     # import statement
     imp = "#import" * "\"" * "@preview/tablex:0.0.8\": tablex, gridx, hlinex, vlinex, colspanx, rowspanx, cellx";
-    println(imp)
+
+    col1width = if isnothing(col1width)
+        mxname
+    else
+        col1width
+    end |> string
 
     # typst variables
     tvars = [
         # "#let topinset = 0.1em",
-        "#let col1width = 12em" * "\n",
-        "#let coliwidth = auto" * "\n \n"
-    ];
+        "// table params \n",
+        "#let col1width = " * col1width * "em" * "\n",
+        "#let coliwidth = auto" * "\n",
+        "#let name_align = left + horizon \n",
+        "\n"
+    ];    
 
-    cells_o = vcat(imp * "\n", tvars .* "", cells_o)
+    cells_o = vcat(
+        imp * "\n",
+        tvars .* "",
+        pstatement,
+        cells_o
+    )
 
     # table label
     lbraw = split(filename, ".")
@@ -93,15 +153,8 @@ end
 export regtable_typ
 
 function regtable_content!(
-    cells, mfos, cnames, modeltitles, stats, understat = "se";
-    pkey = "_Note:_ \$#super[+]p<0.10\$; \$#super[\$star.op\$]p<0.05\$; \$#super[\$star.op star.op\$]p<0.01\$; \$#super[\$star.op star.op star.op\$]p<0.001\$",
-    statnames = Dict( # nice names for common statistics
-        "nobs" => "N",
-        "r2" => "\$R^2\$",
-        "adjr2" => "Adjusted \$R^2\$",
-        "aic" => "AIC",
-        "bic" => "BIC"
-    )
+    cells, mfos, cnames, modeltitles,
+    stats, understat, pvalkey, pnote, statnames
 )
 
     # define colnum as variable name col +  model cols
@@ -146,7 +199,7 @@ function regtable_content!(
 
     for (cn, v) in dd
         # variable name entry
-        push!(cells, cellx(content = cn, align = :left, x = 0, y = v))
+        push!(cells, cellx(content = cn, align = :name_align, x = 0, y = v))
         
         # model values entries
         for (mi, mfo) in enumerate(mfos)
@@ -157,7 +210,7 @@ function regtable_content!(
                     vl = if st == "se"
                         "(" * cinfo["se"] * ")"
                     elseif st == "est"
-                        cinfo[st] * pthres(get(cinfo, "pval", "99"))
+                        cinfo[st] * passign(get(cinfo, "pval", "99"), pvalkey)
                     else
                         cinfo[st]
                     end
@@ -199,7 +252,7 @@ function regtable_content!(
         kn = get(statnames, k, k)
         push!(
             cells,
-            cellx(content = kn, align = :left, x = 0, y = v)
+            cellx(content = kn, align = :name_align, x = 0, y = v)
         )
     end
 
@@ -226,22 +279,24 @@ function regtable_content!(
     # ydex += 1
     push!(
         cells,
-        cellx(; content = pkey, colspan = colnum, align = :left, y = ydex)
+        cellx(; content = pnote, colspan = colnum, align = :left, y = ydex)
     )
 end
 
-function pthres(
-    pv;
-    ky = [
-        "#super[\$+\$]",
-        "#super[\$star.op\$]",
-        "#super[\$star.op star.op\$]",
-        "#super[\$star.op star.op star.op\$]"
-    ]
-)
-    fl = findlast(parse(Float64, pv) .< [0.1, 0.05, 0.01, 0.001])
+"""
+passign()
+
+## Description
+
+Assign p-value symbol based on `pvalkey` and the model p-value.
+
+Assignments are calls to the appropriate position in the typst array `ps`,
+defined at the beginning of the table.
+"""
+function passign(pv, pvalkey)
+    fl = findlast(parse(Float64, pv) .< pvalkey.values)
     return if !isnothing(fl)
-        ky[fl]
+        "#" * "ps.at(" * string(fl-1) * ")" # zero-based in typst
     else
         ""
     end
